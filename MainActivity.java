d1f1409f129096ff041287998975f19e36dd9b90
zain64623 <l217707@lhr.nu.edu.pk>
2025-03-16 17:57:24 +0500

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -16,3 +16,17 @@ public class MainActivity extends AppCompatActivity {
 
     // Handle profile image selection (see next step)
 }
+// Add this method to open gallery
+private void openGallery() {
+    Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+    startActivityForResult(intent, 1);
+}
+
+@Override
+protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+    if (requestCode == 1 && resultCode == RESULT_OK && data != null) {
+        Uri imageUri = data.getData();
+        profileImage.setImageURI(imageUri);
+    }
+}
