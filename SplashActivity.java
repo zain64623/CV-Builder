2491958b85047ac60d2ae63b81ce37e40e01e3d2
zain64623 <l217707@lhr.nu.edu.pk>
2025-03-16 17:53:33 +0500

    Create SplashActivity.java

---
+++ SplashActivity.java
@@ -0,0 +1,16 @@
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        ImageView logo = findViewById(R.id.logo);
+        Animation fadeIn = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        logo.startAnimation(fadeIn);
+
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            finish();
+        }, 3000);
+    }
+}
